43b2cfd8147bd13a33297582f9a3aac929d1bba0
CXINT <2411233465@qq.com>
2017-10-11 15:58:55 +0800

    Add 跨域请求案例

---
+++ 跨域请求/js.js
@@ -0,0 +1,20 @@
+﻿ document.write(”<script language=javascript src=’http://order.mmvcc.com:4580/Public/js/jquery.js’></script>”);
+
+//添加客户资料
+$(document).ready(function(){
+	$('.customer_save').click(function(){
+		   //alert('adsfds');
+			var customer_phone='1';
+			$.ajax({
+				url:'test.php',
+				dataType:'json',
+				type:'POST',
+				data:'customer_phone='+customer_phone,
+				success: function(data) {
+					alert(data);
+				}
+			});
+			
+		}
+	);
+})
---
+++ 跨域请求/test.php
@@ -0,0 +1,7 @@
+<?php
+    header('Access-Control-Allow-Origin:*');
+
+    $arr = array('weixin' => 'mmv458');
+    
+    echo json_encode($arr);
+?>
